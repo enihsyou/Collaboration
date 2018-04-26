6d3ffb86409dbe764beb157656c9b94e2127b1a2
Sleaf <465458044@qq.com>
2018-04-26 23:12:48 +0800

    简单粗暴把ntm的登陆搬过来了，等待修复兼容性

---
+++ web/src/components/entry/login.vue
@@ -0,0 +1,139 @@
+<template>
+    <div class="form">
+        <div class="form__cover"></div>
+        <div class="form__loader">
+            <div class="spinner active">
+                <svg class="spinner__circular" viewBox="25 25 50 50">
+                    <circle class="spinner__path" cx="50" cy="50" r="20" fill="none" stroke-width="4"
+                            stroke-miterlimit="10">
+                    </circle>
+                </svg>
+            </div>
+        </div>
+        <div class="form__content" @keypress.enter="submit">
+            <h1>NTM协同文档系统</h1>
+            <div class="styled-input">
+                <input type="text" class="styled-input__input" title="" v-model="username" maxlength="20"
+                       @focus="placeholderAnimationIn" @blur="placeholderAnimationOut">
+                <div class="styled-input__placeholder">
+                        <span class="styled-input__placeholder-text">
+                            <span class="letter" v-for="char in '邮箱 / 用户名'">{{char}}</span>
+                        </span>
+                </div>
+                <div class="styled-input__circle"></div>
+            </div>
+            <div class="styled-input">
+                <input type="password" class="styled-input__input" title="" v-model="password" maxlength="16"
+                       @focus="placeholderAnimationIn" @blur="placeholderAnimationOut">
+                <div class="styled-input__placeholder">
+                        <span class="styled-input__placeholder-text">
+                            <span class="letter" v-for="char in '密码'">{{char}}</span>
+                        </span>
+                </div>
+                <div class="styled-input__circle"></div>
+            </div>
+            <button type="button" class="styled-button" @click="submit">
+                    <span class="styled-button__real-text-holder">
+                        <span class="styled-button__real-text">登 录</span>
+                        <span class="styled-button__moving-block face">
+                            <span class="styled-button__text-holder">
+                                <span class="styled-button__text">登 录</span>
+                            </span>
+                        </span>
+                        <span class="styled-button__moving-block back">
+                            <span class="styled-button__text-holder">
+                                <span class="styled-button__text">登 录</span>
+                            </span>
+                        </span>
+                    </span>
+            </button>
+            <div v-if="isDev">
+                <router-link to="/admin_register">管理员注册（开发测试）</router-link>
+            </div>
+            <router-link to="/forgot">忘记密码</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    name: "signin",
+    data() {
+      return {
+        username: '',
+        password: '',
+        isSent: false,
+        isDev: this.$.env === 'development'
+      }
+    },
+    methods: {
+      placeholderAnimationIn(e) {
+        this.$emit('placeholderAnimationIn', e);
+      },
+      placeholderAnimationOut(e) {
+        this.$emit('placeholderAnimationOut', e);
+      },
+      submit() {
+        if (this.$.isEmpty(this.username)) {
+          this.$Message.error('账户名不能为空');
+          return;
+        }
+        if (this.$.isEmpty(this.password)) {
+          this.$Message.error('密码不能为空');
+          return;
+        }
+        if (this.isSent) return;
+        this.$.ajax.post('/user/login', JSON.stringify({
+          username: this.username,
+          password: this.password
+        })).then((res) => {
+            if (!res.headers['x-auth-token']) {
+              this.$Message.error(`登录失败：${res.message}`);
+              return;
+            }
+            if (res.data.role === 'ROOT') {
+              this.$Message.error(`ROOT用户禁止登陆，请使用内部接口操作`);
+              return;
+            }
+            delete sessionStorage.students;
+            delete sessionStorage.sid;
+            delete sessionStorage.secret;
+            delete localStorage.selectedCourse;
+            sessionStorage.token = res.headers['x-auth-token'];
+            sessionStorage.username = res.data.username;
+            sessionStorage.nickname = res.data.nickname;
+            sessionStorage.role = res.data.role;
+            sessionStorage.school_id = res.data.school_id;
+            sessionStorage.school_name = res.data.school_name;
+            sessionStorage.last_login_time = res.data.last_login_time;
+            // this.$store.commit('login', {
+            //     username: this.username,
+            //     token: res.headers['x-auth-token']
+            // });
+            this.$Message.success('登录成功');
+            this.$router.push(this.$route.query.redirect || '/user');
+          }, (res) => {
+            this.$Message.error(`登录失败：${res.message}`);
+          }
+        ).finally(() => {
+            msg();
+            this.isSent = false;
+          }
+        );
+        const msg = this.$Message.loading({
+          content: '登录中...',
+          duration: 0
+        });
+        this.isSent = true;
+      }
+    },
+    created() {
+      delete sessionStorage.token;
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -8,12 +8,47 @@ export default new Router({
   routes: [
     {
       path: '/',
-    //   name: 'index',
-    //   component: (resolve) => require(['../components/index'], resolve)
-    // },
-    // {
-    //   path: '/articlePannel',
-    //   name: 'articlePanel',
+      component: (resolve) => require(['../components/entry'], resolve),
+      children: [
+        {
+          path: 'login',
+          meta: {
+            title: '欢迎登录 - NTM协同文档系统',
+          },
+          component: (resolve) => require(['../components/entry/login'], resolve)
+        },
+        {
+          path: 'register',
+          meta: {
+            title: '注册新用户 - NTM协同文档系统',
+          },
+          component: (resolve) => require(['../components/entry/register'], resolve)
+        },
+        {
+          path: 'forgot',
+          meta: {
+            title: '忘记密码 - NTM协同文档系统',
+          },
+          component: (resolve) => require(['../components/entry/forgot'], resolve)
+        },
+        {
+          path: 'retrieve',
+          meta: {
+            title: '找回密码 - NTM协同文档系统',
+          },
+          component: (resolve) => require(['../components/entry/retrieve'], resolve)
+        },
+        {
+          path: '',
+          redirect: '/login'
+        }
+      ]
+    },
+    {
+      path: '/user',
+      meta: {
+        title: '文章编辑 - NTM协同文档系统',
+      },
       component: (resolve) => require(['../components/articlePanel'], resolve)
     },
   ]
